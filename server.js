3a31b482de15d7820509bf1466fe3b31cf438be4
Michael Rao <jmichaelrao@gmail.com>
2021-06-20 16:38:11 -0500

    add constants for repeated text to access public folder and db path

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const util = require("util");
-const writeFileAsync = util.promisify(fs.writeFile);
+// const util = require("util");
+// const writeFileAsync = util.promisify(fs.writeFile);
 const { uuid } = require("uuid");
 
 // Sets up the Express App
@@ -15,38 +15,35 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/static", express.static("./static/"));
 
-let notes = [];
+// shorteners
+const dirPub = path.join(__dirname, "/public");
+const dbPath = "./db/db.json";
 
 // HTML Routes
 // Displays notes page
-app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/notes.html"))
-);
+app.get("/notes", (req, res) => res.sendFile(path.join(dirPub, "notes.html")));
 
 // Displays all notes by returning all data in the database
 app.get("/api/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./db/db.json"));
+  res.sendFile(path.join(__dirname, dbPath));
 });
 
 // Basic route that sends user to the first page
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
-);
+app.get("*", (req, res) => res.sendFile(path.join(dirPub, "index.html")));
 
 // Create new notes and return
 app.post("/api/notes", (req, res) => {
+  let notes = JSON.parse(fs.readFileSync(dbPath, "utf8"));
   let newNote = req.body;
   newNote.id = uuid;
   notes.push(newNote);
   // pushes updated data to database
-  writeFileAsync(path.join(__dirname, "./db/db.json"), JSON.stringify(notes));
-  res.json(newNote);
+  fs.writeFileSync(dbpath, JSON.stringify(notes));
+  res.json(notes);
 });
 
 // Deletes notes
-app.delete("/api/notes", (req, res) => {
-  
-})
+// app.delete("/api/notes", (req, res) => {});
 
 // Starts the server to begin listening
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
