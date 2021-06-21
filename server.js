8800a5c41fabbef74ff09dbc233820aec0061745
Michael Rao <jmichaelrao@gmail.com>
2021-06-20 22:06:31 -0500

    debugging heroku deployment

--- server.js
+++ server.js
@@ -1,48 +1,55 @@
-// dependecies
+// Dependencies
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
 const { v4: uuidv4 } = require("uuid");
 
 // Sets up the Express App
 const app = express();
-const PORT = process.env.port || 8080;
+const PORT = process.env.PORT || 3001;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/static", express.static("./static/"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-// shorteners
-const dirPub = path.join(__dirname, "/public");
-const dbPath = "./db/db.json";
-let notes = JSON.parse(fs.readFileSync(dbPath, "utf8"));
+let notes = [];
 
-// HTML Routes
-// Displays notes page
-app.get("/notes", (req, res) => res.sendFile(path.join(dirPub, "notes.html")));
+// Routes
 
-// Displays all notes by returning all data in the database
-app.get("/api/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, dbPath));
-});
+// route that gets user to the notes page
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
+);
 
-// Basic route that sends user to the first page
-app.get("*", (req, res) => res.sendFile(path.join(dirPub, "index.html")));
+// Displays all notes
+// read file async
+app.get("/api/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "db/db.json"))
+);
 
-// API routes
-// Create new notes and return
+// saves new note to test database not JSON file
 app.post("/api/notes", (req, res) => {
-  let newNote = req.body;
+  const newNote = req.body;
   newNote.id = uuidv4();
   notes.push(newNote);
-  // pushes updated data to database
-  fs.writeFileSync(dbPath, JSON.stringify(notes));
-  res.json(notes);
+  writeFileAsync(path.join(__dirname, "db/db.json"), JSON.stringify(notes));
+  res.json(newNote);
 });
 
-// Deletes notes
-app.delete("/api/notes/:id", (req, res) => {});
+// delete route
+app.delete("/api/notes/:id", (req, res) => {
+  notes = notes.filter((note) => note.id !== req.params.id);
+  writeFileAsync(path.join(__dirname, "db/db.json"), JSON.stringify(notes));
+  res.sendStatus(200);
+});
+
+// route that gets user to homepage
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/index.html"))
+);
 
 // Starts the server to begin listening
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
